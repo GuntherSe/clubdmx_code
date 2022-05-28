ad9f5b02d9a6c672d6e137689b961032f0cbe5b2
GuntherSe <gunther.seiser.63@gmail.com>
2022-05-28 23:24:38 +0200

    update sendosc

--- sendosc.sh
+++ sendosc.sh
@@ -12,4 +12,4 @@
 codepath="${CLUBDMX_CODEPATH:-/home/pi/clubdmx_code}"
 cd $codepath
 
-python3 dmx/sendosc.py $1 $2 $3 $4 $5 $6 $7 $8 $9 
+python3 dmx/sendosc.py $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12}
